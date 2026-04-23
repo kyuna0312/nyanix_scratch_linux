fcab6a387d55843f6c6108b8ccbc5a6e95b8ec16
kyuna0312 <khatanzorigb@gmail.com>
2026-04-23 11:11:06 +0900

    build script changes

--- build.sh
+++ build.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
-# Build Nyanix kernel
+# Build Nyanix (delegates to Makefile)
 set -e
 
-mkdir -p boot/bin
-
-echo "Assembling bootloader..."
-nasm boot/boot.asm -f bin -o boot/bin/boot.bin
-
-echo "Assembling kernel entry..."
-nasm boot/kernel_entry.asm -f elf -o boot/bin/kernel_entry.o
-
-echo "Compiling kernel..."
-gcc -m32 -ffreestanding -fno-stack-protector -nostdlib \
-    -fno-pic -fno-pie -fno-asynchronous-unwind-tables \
-    -Wno-write-strings \
-    -Iboot \
-    -c boot/final.c -o boot/bin/final.o
-
-echo "Linking kernel..."
-ld -m elf_i386 -T boot/kernel.ld \
-    -o boot/bin/kernel.elf \
-    boot/bin/kernel_entry.o boot/bin/final.o
-
-echo "Creating image..."
-objcopy -O binary boot/bin/kernel.elf boot/bin/kernel.bin
-cat boot/bin/boot.bin boot/bin/kernel.bin > os.img
-
-echo "Done: os.img ($(ls -lh os.img | awk '{print $5}'))"
+make
+echo "Built: x86_64 + ARM64"
--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Run Nyanix in QEMU (auto-exits after 60s)
-./build.sh
-qemu-system-x86_64 -drive format=raw,file=os.img -m 32M -vga std -display gtk &
-sleep 60
-echo "Done (60s timeout)"
+# Run Nyanix (delegates to Makefile)
+make run
